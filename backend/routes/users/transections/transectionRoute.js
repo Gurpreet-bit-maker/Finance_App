44f4f525f084c9b0c0b8c1b0c312af6f92bd04e5
Gurpreet <ajbagdi6@gmail.com>
2026-09-07 16:10:06 +0530

    new big fixed

--- backend/routes/users/transections/transectionRoute.js
+++ backend/routes/users/transections/transectionRoute.js
@@ -9,8 +9,9 @@ import { isValidToken } from "../../../middleware/user/tokenMiddleware.js";
 router.post("/api/user/create-income", isValidToken, incomeController);
 router.post("/api/user/create-expense", isValidToken, addExpenseController);
 router.get("/api/user/dashboard", isValidToken, transections);
-router.get("/api/user/health", () => {
+router.get("/api/user/health", (req, res) => {
   console.log("working this api");
+  return res.status(200).json("working");
 });
 
 export default router;
